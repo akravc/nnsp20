8bac70095a067ddcf0b3078940ca57346ff7e746
Anastasiya Kravchuk-Kirilyuk <nastena95@gmail.com>
2020-05-05 22:51:53 -0400

    removed old file

---
+++ script.py
@@ -0,0 +1,11 @@
+trace = open('wiki2018.tr', 'r')
+# writing to file 
+output = open('50t.txt', 'w')
+
+while True:
+  line = trace.readline()
+  if 
+  output.writelines(line)
+  
+output.close()
+trace.close()
